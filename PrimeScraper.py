53c1e4ab04c9580353537b6c6b448faa79ee63b9
Zachary Hunt <=>
2020-05-21 17:42:07 -0500

    prime unzipping

--- PrimeScraper.py
+++ PrimeScraper.py
@@ -1,16 +1,25 @@
+import os
 import requests
 from zipfile import ZipFile
 
 
 def main(redownload=False):
-    # TODO: Check if files exist, then unzip!
+    zips_range = (1, 50 + 1)
     if redownload:
-        download_prime_zips()
+        refetch = range(*zips_range)
+    else:
+        refetch = []
+        for i in range(*zips_range):
+            filename = "./primezips/" + str(i) + ".zip"
+            if not os.path.isfile(filename) or os.stat(filename).st_size < 1500:
+                refetch.append(i)
+    download_prime_zips(portion=refetch)
+    unzip_primes(portion=range(*zips_range))
     print("Done!")
 
 
-def download_prime_zips(save_path="./primezips/", portion=(1, 50)):
-    for i in range(portion[0], portion[1] + 1):
+def download_prime_zips(save_path="./primezips/", portion=range(1, 50 + 1)):
+    for i in portion:
         url = r"https://primes.utm.edu/lists/small/millions/primes" + str(i) + ".zip"
         try:
             r = requests.get(url, stream=True, verify=False)
@@ -21,5 +30,13 @@ def download_prime_zips(save_path="./primezips/", portion=(1, 50)):
             f.write(r.content)
 
 
+def unzip_primes(portion=range(1, 50 + 1)):
+    old_path = "./primezips/"
+    new_path = "./primetables/"
+    for i in portion:
+        with ZipFile(old_path + str(i) + ".zip") as zfile:
+            zfile.extract("primes" + str(i) + ".txt", path=new_path)
+
+
 if __name__ == "__main__":
-    main()
+    main(redownload=False)
